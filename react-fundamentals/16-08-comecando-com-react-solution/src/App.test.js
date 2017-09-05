a160a21da7e6b15f89eb7e4a102301b9c4d1c0d7
Fernando Terra <nandobrt@gmail.com>
2017-09-05 13:12:30 -0300

    *using jest *install enzyme (and react-test-renderer), enzyme-to-json note: snapshot before enzyme-to-json

--- react-fundamentals/16-08-comecando-com-react-solution/src/App.test.js
+++ react-fundamentals/16-08-comecando-com-react-solution/src/App.test.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
+global.localStorage = {
+  getItem: () => {}
+}
+
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  expect(shallow(<App />)).toMatchSnapshot();
 });
